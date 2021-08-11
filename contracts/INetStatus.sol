3dde33d9c8d3d10f0744c18db0a21c3db672bafa
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-08-11 23:32:58 +0800

    Delete INetStatus.sol

--- contracts/INetStatus.sol
+++
@@ -1,9 +0,0 @@
-interface INetStatus {
-    function addSiteDetails(
-            bytes32 _siteName,
-            string memory _tag, 
-            string memory _siteType, 
-            string memory _siteSafety,
-            address operator
-        ) external;
-}
